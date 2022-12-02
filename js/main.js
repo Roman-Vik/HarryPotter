93ded7717b03438470d67b309ff8cc2f2b8ce445
Roman Simonov <roman.viktorovich@outlook.com>
2022-12-02 12:38:58 +0300

    select school

--- js/main.js
+++ js/main.js
@@ -2,24 +2,33 @@ import data from './data.js'
 import { createCard } from './createCard.js';
 
 
-const deckCards = document.querySelector('.deck__cards');
+const deckCards = document.querySelector('.deck__cards')
+
+const scanSelect = document.getElementById('school')
+
+function func(event) {
+  let school = event.target.value;
+  console.log(school)
+  
+}
+
+scanSelect.addEventListener('click', func  )
+
+
+
 
 
 data.forEach(el => deckCards.append(createCard(el)));
 
 const scanName = document.querySelector('.formFilter__field');
-//console.log(scanName)
-
-
-
 
 const searchName = () => {
+
   let filterData = data.filter((el, i) => {
   return el.name.toLowerCase().includes(scanName.value) 
   });
   deckCards.innerHTML = '';
   filterData.forEach(el => deckCards.append(createCard(el)))
- 
 }
 
 scanName.addEventListener('input', searchName)
